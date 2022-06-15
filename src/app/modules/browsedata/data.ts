b2e8e496f2d84794b29189a30799ba69f6980dee
Ayaz Alavi <ayaz@pursue.today>
2022-06-15 20:49:31 +0500

    dummy data for browse page

---
+++ src/app/modules/browsedata/data.ts
@@ -0,0 +1,43 @@
+
+export type Person = {
+  firstName: string
+  lastName: string
+  age: number
+  visits: number
+  progress: number
+  status: 'relationship' | 'complicated' | 'single'
+  subRows?: Person[]
+}
+
+const range = (len: number) => {
+  const arr = []
+  for (let i = 0; i < len; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+const newPerson = (): Person => {
+  return {
+    firstName: "name",
+    lastName: "last",
+    age: 20,
+    visits: 1000,
+    progress: 40,
+    status: 'single'
+  }
+}
+
+export function makeData(...lens: number[]) {
+  const makeDataLevel = (depth = 0): Person[] => {
+    const len = lens[depth]!
+    return range(len).map((d): Person => {
+      return {
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+      }
+    })
+  }
+
+  return makeDataLevel()
+}
