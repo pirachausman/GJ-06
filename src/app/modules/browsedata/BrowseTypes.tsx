4125329be15198c1292ee739f63de76bdeb4e4e9
Ayaz Alavi <ayaz@pursue.today>
2022-06-15 20:48:40 +0500

    added browse types

---
+++ src/app/modules/browsedata/BrowseTypes.tsx
@@ -0,0 +1,10 @@
+export interface BrowseFormType {
+  segment: boolean
+  state: string
+  electionType: string
+}
+
+export interface AssemblyType {
+  assemblyNumber: number
+  year: string | undefined
+}
